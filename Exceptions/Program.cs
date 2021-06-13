64af9bf98f8335f7bcd91b07d28cb4d0afae7068
Ömer Serfice <49249870+omerserfice@users.noreply.github.com>
2021-06-13 23:26:32 +0300

    updated

---
+++ Exceptions/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ExceptionIntro();
+            try
+            {
+                Find();
+            }
+            catch (RecordNotFoundException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            HandleException(() => 
+            {
+                Find();
+            });
+
+            Console.ReadLine();
+
+        }
+
+        private static void HandleException(Action action)
+        {
+            try
+            {
+                action.Invoke();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                
+            }
+        }
+
+        private static void Find()
+        {
+            List<string> students = new List<string>() { "Ömer", "Ali", "Osman" };
+            if (!students.Contains("Ahmet"))
+            {
+                throw new RecordNotFoundException("Record Not Found");
+            }
+            else
+            {
+                Console.WriteLine("Record Found!");
+            }
+        }
+
+        private static void ExceptionIntro()
+        {
+            try
+            {
+                string[] students = new string[3]
+                {"Ömer","Ali","Osman" }
+                ;
+                students[3] = "ahmet";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.InnerException);
+
+            }
+        }
+    }
+}
